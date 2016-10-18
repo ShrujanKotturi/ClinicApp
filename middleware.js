0327e1d7ba71bfd0139a43997f18fb582f87d248
Shrujan Kotturi <skotturi@uncc.edu>
2016-10-18 15:46:43 -0400

    Adding FCM

--- middleware.js
+++ middleware.js
@@ -15,19 +15,6 @@ function requireAuthentication (req, res, next) {
 
 }
 
-function Authenticate(token){
-    return new Promise(function(resolve, reject){
-        try{
-            var decodedJWT = jwt.verify(token, 'qwerty098');
-            var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
-            var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
-            console.log("auth token id " + tokenData);
-            resolve(tokenData);
-        }catch(err){
-            reject();
-        }
-    });
-}
 
 function requireAdminAuthentication (req, res, next) {
     var token = req.get('Auth');
@@ -44,24 +31,10 @@ function requireAdminAuthentication (req, res, next) {
 
 function MiddleWare(){
 
-    this.Authenticate = function(token){
-        return new Promise(function(resolve, reject){
-            try{
-                var decodedJWT = jwt.verify(token, 'qwerty098');
-                var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
-                var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
-                console.log("auth token id " + tokenData);
-                resolve(tokenData);
-            }catch(err){
-                reject();
-            }
-        });
-    }
-
     this.RequireAuthentication = function(req, res, next){
         var token = req.get('Auth');
 
-        this.Authenticate(token).then(function (tokenData){
+        Authenticate(token).then(function (tokenData){
             res.locals.user = tokenData.UserId;
             console.log("auth token id " + res.locals.user);
             next();
@@ -69,18 +42,32 @@ function MiddleWare(){
             console.log(err);
             res.status(401).send();
         });
-    }
+    };
 
     this.RequireAdminAuthentication = function(req, res, next){
         var token = req.get('Auth');
 
-        this.Authenticate(token).then(function (){
+        Authenticate(token).then(function (){
             next();
         }, function () {
             console.log(err);
             res.status(401).send();
         });
-    }
+    };
+}
+
+function Authenticate(token){
+    return new Promise(function(resolve, reject){
+        try{
+            var decodedJWT = jwt.verify(token, 'qwerty098');
+            var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
+            var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+            console.log("auth token id " + tokenData);
+            resolve(tokenData);
+        }catch(err){
+            reject();
+        }
+    });
 }
 
 module.exports = new MiddleWare();
